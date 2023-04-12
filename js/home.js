fbada0b175a77d721f8c99c4fcea117781b5f1a6
MichaelMagdyKhairElSaid <michaelkhairelsaid@gmail.com>
2023-04-12 15:31:39 +0200

    second commit

--- js/home.js
+++ js/home.js
@@ -1,3 +1,7 @@
 let UserName= document.querySelector("#UserName")
+if (localStorage.getItem("sessionUserName")==null) {
+    window.open("index.html","_self")
+}else{
 console.log(`localStorage.getItem("sessionUserName")  = `+localStorage.getItem("sessionUserName"));
 UserName.innerHTML="Welcome "+ localStorage.getItem("sessionUserName")
+}
